449efb8e71959b76e756a4d563351a3aabf75206
ywyz <admin@ywyz.tech>
2021-05-04 19:24:16 +0800

    数据导入到数据库中

--- importdata.py
+++ importdata.py
@@ -2,21 +2,23 @@ import pymysql
 from openpyxl import load_workbook
 db = pymysql.connect(host="localhost", user="root", password="yw980924@163.com", database="student_information")
 cursor = db.cursor()
+sqlDelete = "truncate table student_basic"
+cursor.execute(sqlDelete)
 filename = input("请拖入文件：")
 wb = load_workbook(filename=filename)
 sheetName = "学生"
 ws = wb[sheetName]
 
 for row in ws.rows:
-    if ws.cell(row, 1).value == "序号":
+    if row[0].value == "序号":
         continue
     else:
-        name = row[4].value
-        parentName = row[5].value
-        parentNumber = row[6].value
-        Grade = row[8].value
-        Class = row[9].value
-        Address = row[12].value
+        name = row[3].value
+        parentName = row[4].value
+        parentNumber = row[5].value
+        Grade = row[7].value
+        Class = row[8].value
+        Address = row[11].value
         SqlInsert = "INSERT INTO student_basic (studentname, grade, class, parentName_One, parentPhone_one, address) \
                     VALUES('%s', '%s', '%s','%s', '%s', '%s') " % \
                     (name, Grade, Class, parentName, parentNumber, Address)
